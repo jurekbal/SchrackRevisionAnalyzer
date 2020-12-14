9fdd964674128d906db5dbfd417e7e1cbbb5618e
Jerzy Balwinski <jurek_b@wp.pl>
2020-12-14 20:49:22 +0100

    Tested detectors database object (Map) with parsed test data as values

--- src/main/java/com/balwinski/sra/Main.java
+++ src/main/java/com/balwinski/sra/Main.java
@@ -1,15 +1,16 @@
 package com.balwinski.sra;
 
+import com.balwinski.sra.model.TestingData;
+import com.balwinski.sra.model.Entry;
 import com.balwinski.sra.model.ReportFile;
 import com.balwinski.sra.services.FileParser;
 
 import java.nio.file.Paths;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
-    private static final String pathStringToTestFile = "d:\\javafiles\\schrack\\sratest.txt";
-    private static final String pathStringToTestDir = "d:\\javafiles\\schrack\\txt";
+    private static final String pathStringToTestDir = "d:\\javafiles\\schrack\\dbtest";
 
     public static void main(String[] args) {
 
@@ -21,11 +22,42 @@ public class Main {
 //                .map(ReportFile::getPath)
 //                .forEach(System.out::println);
 
+        // creating database with group/detector key and list of testing data
+        Map<String, List<TestingData>> db = new HashMap<>();
+        for (ReportFile file : reportFiles) {
+            List<Entry> entries = file.getEntries();
+            for (Entry entry : entries) {
+                String key = entry.getKey();
+                List<TestingData> testingDataList = db.get(key);
+                if (testingDataList == null) {
+                    //detector first encounter - create new entry in db
+                    db.put(key, new ArrayList<>(Collections.singletonList(new TestingData(file, entry))));
+                } else {
+                    //update testingDataList
+                    testingDataList.add(new TestingData(file, entry));
+                }
+
+            }
+        }
+
+        System.out.println("Database size: " + db.size() + " entries.");
+//        Set<String> keySet = db.keySet();
+//        keySet.stream().sorted().limit(100).forEach(System.out::println);
+        db.entrySet().stream().limit(10).forEach(Main::printDbEntry);
+
         //TODO next
-        // detectors database (Map)
+        // detectors database (Map) - DONE!
         // test database, extract not tested detectors
         // list of invalid text files found in directory tree - to log in summary
         // tests and test methods (Header parser, ...)
         // getting base path from execute location and/or args
+    }
+
+    private static void printDbEntry(Map.Entry<String, List<TestingData>> dbEntry) {
+        System.out.print(dbEntry.getKey() + ":");
+        dbEntry.getValue().forEach(testingData ->
+                System.out.println("\t"+testingData.getFile().getPath() +
+                        " Checked: " + testingData.getEntry().getCheckedCount())
+        );
     }
 }
---
+++ src/main/java/com/balwinski/sra/model/TestingData.java
@@ -0,0 +1,12 @@
+package com.balwinski.sra.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class TestingData {
+
+    ReportFile file;
+    Entry entry;
+}
